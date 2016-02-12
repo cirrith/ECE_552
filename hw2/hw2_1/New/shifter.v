804c5bc9009f0cc9547d773eaac22c22af7642d9
rbambrough <Ryan.bambrough@gmail.com>
2016-02-12 14:31:49 -0600

    Lock

--- hw2/hw2_1/New/shifter.v
+++ hw2/hw2_1/New/shifter.v
@@ -5,9 +5,14 @@ module shifter (In, Cnt, Op, Out);
    input [1:0]  Op;
    output [15:0] Out;
 
-   /*
-   Your code goes here
-   */
+   wire [15:0] tran1_2;
+   wire [15:0] tran2_4;
+   wire [15:0] tran4_8;
+   
+   shift1 sh1 (.in(In),      .op(Op), .sh(Cnt[0]), .out(tran1_2));
+   shift2 sh2 (.in(tran1_2), .op(Op), .sh(Cnt[1]), .out(tran2_4));
+   shift4 sh4 (.in(tran2_4), .op(Op), .sh(Cnt[2]), .out(tran4_8));
+   shift8 sh8 (.in(tran4_8), .op(Op), .sh(Cnt[3]), .out(Out));
    
 endmodule
 
