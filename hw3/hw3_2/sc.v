13d462219c6374da9e0556d17e69183f4a665b88
Ryan Christopher Bambrough <rbambrough@tux-13.cae.wisc.edu>
2016-02-19 15:21:35 -0600

    Problem 2 Working

--- hw3/hw3_2/sc.v
+++ hw3/hw3_2/sc.v
@@ -9,7 +9,53 @@ module sc( clk, rst, ctr_rst, out, err);
    output [2:0] out;
    output err;
 
-   // your code
+   reg [2:0] state = 0;
+
+   reg error;
+   
+   assign out = state;
+   
+   assign err = error;
+   
+   always@( posedge clk) begin
+	case ({rst,state})
+		0: begin
+				state <= !ctr_rst ? (state + 1) : 0;
+				error <= 0;
+			end
+		1: begin
+				state <= !ctr_rst ? (state + 1) : 0;
+				error <= 0;
+			end
+		2: begin
+				state <= !ctr_rst ? (state + 1) : 0;
+				error <= 0;
+			end
+		3: begin
+				state <= !ctr_rst ? (state + 1) : 0;
+				error <= 0;
+			end
+		4: begin
+				state <= !ctr_rst ? (state + 1) : 0;
+				error <= 0;
+			end
+		5: begin
+				state <= !ctr_rst ? state : 0;
+				error <= 0;
+			end
+		6: begin
+			error <= 1;
+			$display ("Entered Wrong State");
+			end
+		7: begin 
+			error <= 1;
+			$display ("Entered Wrong State");
+			end
+		default: begin
+			state <= 0;
+		end
+	endcase
+	end
 endmodule
 
 // DUMMY LINE FOR REV CONTROL :1:
