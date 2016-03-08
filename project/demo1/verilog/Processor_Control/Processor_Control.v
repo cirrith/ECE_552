6eced30b05cfa4c05aaa5696678197d2346c5ab6
Cirrith <rbambrough@tux-70.cae.wisc.edu>
2016-03-08 17:05:16 -0600

    PC Update

--- project/demo1/verilog/Processor_Control/Processor_Control.v
+++ project/demo1/verilog/Processor_Control/Processor_Control.v
@@ -53,7 +53,7 @@ input [4:0] OP_Code;
 output PC_Write;
 output [2:0] PC_Code; 			//Determine
 output [1:0] Comp_Code;
-output [1:0] Write_Back_Sel; 	//Determine
+output Write_Back_Sel; 			//Determine
 output Mem_Write;
 output Mem_Read;
 output [3:0] ALU_OP; 			//Determine
@@ -82,6 +82,38 @@ always @ (OP_Code) begin
 	PC_Code = 6; //Nothing
 	PC_Code = 7; //Nothing
 	
+	Comp_Code = 0; //Equal
+	Comp_Code = 1; //!Equal
+	Comp_Code = 2; //Less Than
+	Comp_Code = 3; //Greater Than or Equal
+	
+	Write_Back_Sel = 0; //ALU
+	Write_Back_Sel = 1; //Memory
+	
+	ALU_OP = 0 // Add
+	ALU_OP = 1 // Subtract
+	ALU_OP = 2 // XOR
+	ALU_OP = 3 // ANDN
+	ALU_OP = 4 // Rotate Left
+	ALU_OP = 5 // Shift Left Logical
+	ALU_OP = 6 // Rotate Right
+	ALU_OP = 7 // Shift Right Logical
+	ALU_OP = 8 // Equal
+	ALU_OP = 9 // Less Than
+	ALU_OP = 10 // Less Than or Equal
+	ALU_OP = 11 // Carry Out
+	ALU_OP = 12 // Flip
+	ALU_OP = 13 // Pass Through
+	ALU_OP = 14 // SLBI
+	ALU_OP = 15 // NOT USED
+	
+	ALU_Src = 0; // Register
+	ALU_Src = 1; // Immediate
+	
+	Write_Reg_Sel = 0; //Rd (Immediate)
+	Write_Reg_Sel = 1; //Rd (Register)
+	Write_Reg_Sel = 2; //Rs (LBI)
+	Write_Reg_Sel = 3; //R7
 	
 	case(OP_Code)
 		5'b00000: begin //HALT
@@ -110,243 +142,243 @@ always @ (OP_Code) begin
 		end
 		5'b01000: begin //ADDI
 			PC_Write = 1;
-			PC_Code = 0;
-			Comp_Code = 0;
-			Write_Back_Sel = 0;
-			Mem_Write = 0;
-			Mem_Read = 0;
+			//PC_Code = 0;
+			//Comp_Code = 0;
+			//Write_Back_Sel = 0;
+			//Mem_Write = 0;
+			//Mem_Read = 0;
 			ALU_OP = 0;
-			ALU_Src = 0;
-			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			ALU_Src = 1;
+			Reg_Write = 1;
+			//Write_Reg_Sel = 0;
 		end
         5'b01001: begin //SUBI
 			PC_Write = 1;
-			PC_Code = 0;
-			Comp_Code = 0;
-			Write_Back_Sel = 0;
-			Mem_Write = 0;
-			Mem_Read = 0;
-			ALU_OP = 0;
-			ALU_Src = 0;
-			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			//PC_Code = 0;
+			//Comp_Code = 0;
+			//Write_Back_Sel = 0;
+			//Mem_Write = 0;
+			//Mem_Read = 0;
+			ALU_OP = 1;
+			ALU_Src = 1;
+			Reg_Write = 1;
+			//Write_Reg_Sel = 0;
 		end
 		5'b01010: begin //XORI
 			PC_Write = 1;
-			PC_Code = 0;
-			Comp_Code = 0;
-			Write_Back_Sel = 0;
-			Mem_Write = 0;
-			Mem_Read = 0;
-			ALU_OP = 0;
-			ALU_Src = 0;
-			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			//PC_Code = 0;
+			//Comp_Code = 0;
+			//Write_Back_Sel = 0;
+			//Mem_Write = 0;
+			//Mem_Read = 0;
+			ALU_OP = 2;
+			ALU_Src = 1;
+			Reg_Write = 1;
+			//Write_Reg_Sel = 0;
 		end
         5'b01011: begin //ANDNI
 			PC_Write = 1;
-			PC_Code = 0;
-			Comp_Code = 0;
-			Write_Back_Sel = 0;
-			Mem_Write = 0;
-			Mem_Read = 0;
-			ALU_OP = 0;
-			ALU_Src = 0;
-			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			//PC_Code = 0;
+			//Comp_Code = 0;
+			//Write_Back_Sel = 0;
+			//Mem_Write = 0;
+			//Mem_Read = 0;
+			ALU_OP = 3;
+			ALU_Src = 1;
+			Reg_Write = 1;
+			//Write_Reg_Sel = 0;
 		end
 		5'b10100: begin //ROLI
 			PC_Write = 1;
-			PC_Code = 0;
-			Comp_Code = 0;
-			Write_Back_Sel = 0;
-			Mem_Write = 0;
-			Mem_Read = 0;
-			ALU_OP = 0;
-			ALU_Src = 0;
-			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			//PC_Code = 0;
+			//Comp_Code = 0;
+			//Write_Back_Sel = 0;
+			//Mem_Write = 0;
+			//Mem_Read = 0;
+			ALU_OP = 4;
+			ALU_Src = 1;
+			Reg_Write = 1;
+			//Write_Reg_Sel = 0;
 		end
         5'b10101: begin //SLLI
 			PC_Write = 1;
-			PC_Code = 0;
-			Comp_Code = 0;
-			Write_Back_Sel = 0;
-			Mem_Write = 0;
-			Mem_Read = 0;
-			ALU_OP = 0;
-			ALU_Src = 0;
-			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			//PC_Code = 0;
+			//Comp_Code = 0;
+			//Write_Back_Sel = 0;
+			//Mem_Write = 0;
+			//Mem_Read = 0;
+			ALU_OP = 5;
+			ALU_Src = 1;
+			Reg_Write = 1;
+			//Write_Reg_Sel = 0;
 		end
 		5'b10110: begin //RORI
 			PC_Write = 1;
-			PC_Code = 0;
-			Comp_Code = 0;
-			Write_Back_Sel = 0;
-			Mem_Write = 0;
-			Mem_Read = 0;
-			ALU_OP = 0;
-			ALU_Src = 0;
-			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			//PC_Code = 0;
+			//Comp_Code = 0;
+			//Write_Back_Sel = 0;
+			//Mem_Write = 0;
+			//Mem_Read = 0;
+			ALU_OP = 6;
+			ALU_Src = 1;
+			Reg_Write = 1;
+			//Write_Reg_Sel = 0;
 		end
         5'b10111: begin //SRLI
 			PC_Write = 1;
-			PC_Code = 0;
-			Comp_Code = 0;
-			Write_Back_Sel = 0;
-			Mem_Write = 0;
-			Mem_Read = 0;
-			ALU_OP = 0;
-			ALU_Src = 0;
-			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			//PC_Code = 0;
+			//Comp_Code = 0;
+			//Write_Back_Sel = 0;
+			//Mem_Write = 0;
+			//Mem_Read = 0;
+			ALU_OP = 7;
+			ALU_Src = 1;
+			Reg_Write = 1;
+			//Write_Reg_Sel = 0;
 		end
-		5'b10000: begin //ST
+		5'b10000: begin //ST !
 			PC_Write = 1;
-			PC_Code = 0;
-			Comp_Code = 0;
-			Write_Back_Sel = 0;
-			Mem_Write = 0;
-			Mem_Read = 0;
+			//PC_Code = 0;
+			//Comp_Code = 0;
+			//Write_Back_Sel = 0;
+			Mem_Write = 1;
+			//Mem_Read = 0;
 			ALU_OP = 0;
-			ALU_Src = 0;
-			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			ALU_Src = 1;
+			//Reg_Write = 0;
+			//Write_Reg_Sel = 0;
 		end
         5'b10001: begin //LD
 			PC_Write = 1;
-			PC_Code = 0;
-			Comp_Code = 0;
-			Write_Back_Sel = 0;
-			Mem_Write = 0;
-			Mem_Read = 0;
+			//PC_Code = 0;
+			//Comp_Code = 0;
+			Write_Back_Sel = 1;
+			//Mem_Write = 0;
+			Mem_Read = 1;
 			ALU_OP = 0;
-			ALU_Src = 0;
-			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			ALU_Src = 1;
+			Reg_Write = 1;
+			//Write_Reg_Sel = 0;
 		end
 		5'b10011: begin //STU
 			PC_Write = 1;
-			PC_Code = 0;
-			Comp_Code = 0;
-			Write_Back_Sel = 0;
-			Mem_Write = 0;
-			Mem_Read = 0;
+			//PC_Code = 0;
+			//Comp_Code = 0;
+			Write_Back_Sel = 2;
+			Mem_Write = 1;
+			//Mem_Read = 0;
 			ALU_OP = 0;
-			ALU_Src = 0;
-			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			ALU_Src = 1;
+			Reg_Write = 1;
+			Write_Reg_Sel = 2;
 		end
         5'b11001: begin //BTR
 			PC_Write = 1;
-			PC_Code = 0;
-			Comp_Code = 0;
-			Write_Back_Sel = 0;
-			Mem_Write = 0;
-			Mem_Read = 0;
-			ALU_OP = 0;
-			ALU_Src = 0;
-			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			//PC_Code = 0;
+			//Comp_Code = 0;
+			//Write_Back_Sel = 0;
+			//Mem_Write = 0;
+			//Mem_Read = 0;
+			ALU_OP = 12;
+			//ALU_Src = 0;
+			Reg_Write = 1;
+			Write_Reg_Sel = 1;
 		end
 		5'b11011: begin //ADD
 			PC_Write = 1;
-			PC_Code = 0;
-			Comp_Code = 0;
-			Write_Back_Sel = 0;
-			Mem_Write = 0;
-			Mem_Read = 0;
+			//PC_Code = 0;
+			//Comp_Code = 0;
+			//Write_Back_Sel = 0;
+			//Mem_Write = 0;
+			//Mem_Read = 0;
 			ALU_OP = 0;
-			ALU_Src = 0;
-			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			//ALU_Src = 0;
+			Reg_Write = 1;
+			Write_Reg_Sel = 1;
 		end
         5'b11011: begin //SUB
 			PC_Write = 1;
-			PC_Code = 0;
-			Comp_Code = 0;
-			Write_Back_Sel = 0;
-			Mem_Write = 0;
-			Mem_Read = 0;
-			ALU_OP = 0;
-			ALU_Src = 0;
-			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			//PC_Code = 0;
+			//Comp_Code = 0;
+			//Write_Back_Sel = 0;
+			//Mem_Write = 0;
+			//Mem_Read = 0;
+			ALU_OP = 1;
+			//ALU_Src = 0;
+			Reg_Write = 1;
+			Write_Reg_Sel = 1;
 		end
 		5'b11011: begin //XOR
 			PC_Write = 1;
-			PC_Code = 0;
-			Comp_Code = 0;
-			Write_Back_Sel = 0;
-			Mem_Write = 0;
-			Mem_Read = 0;
-			ALU_OP = 0;
-			ALU_Src = 0;
-			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			//PC_Code = 0;
+			//Comp_Code = 0;
+			//Write_Back_Sel = 0;
+			//Mem_Write = 0;
+			//Mem_Read = 0;
+			ALU_OP = 2;
+			//ALU_Src = 0;
+			Reg_Write = 1;
+			Write_Reg_Sel = 1;
 		end
         5'b11011: begin //ANDN
 			PC_Write = 1;
-			PC_Code = 0;
-			Comp_Code = 0;
-			Write_Back_Sel = 0;
-			Mem_Write = 0;
-			Mem_Read = 0;
-			ALU_OP = 0;
-			ALU_Src = 0;
-			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			//PC_Code = 0;
+			//Comp_Code = 0;
+			//Write_Back_Sel = 0;
+			//Mem_Write = 0;
+			//Mem_Read = 0;
+			ALU_OP = 3;
+			//ALU_Src = 0;
+			Reg_Write = 1;
+			Write_Reg_Sel = 1;
 		end
 		5'b11010: begin //ROL
 			PC_Write = 1;
-			PC_Code = 0;
-			Comp_Code = 0;
-			Write_Back_Sel = 0;
-			Mem_Write = 0;
-			Mem_Read = 0;
-			ALU_OP = 0;
-			ALU_Src = 0;
-			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			//PC_Code = 0;
+			//Comp_Code = 0;
+			//Write_Back_Sel = 0;
+			//Mem_Write = 0;
+			//Mem_Read = 0;
+			ALU_OP = 4;
+			//ALU_Src = 0;
+			Reg_Write = 1;
+			Write_Reg_Sel = 1;
 		end
 		5'b11010: begin //SLL
 			PC_Write = 1;
-			PC_Code = 0;
-			Comp_Code = 0;
-			Write_Back_Sel = 0;
-			Mem_Write = 0;
-			Mem_Read = 0;
-			ALU_OP = 0;
-			ALU_Src = 0;
-			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			//PC_Code = 0;
+			//Comp_Code = 0;
+			//Write_Back_Sel = 0;
+			//Mem_Write = 0;
+			//Mem_Read = 0;
+			ALU_OP = 5;
+			//ALU_Src = 0;
+			Reg_Write = 1;
+			Write_Reg_Sel = 1;
 		end
 		5'b11010: begin //ROR
 			PC_Write = 1;
-			PC_Code = 0;
-			Comp_Code = 0;
-			Write_Back_Sel = 0;
-			Mem_Write = 0;
-			Mem_Read = 0;
-			ALU_OP = 0;
-			ALU_Src = 0;
-			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			//PC_Code = 0;
+			//Comp_Code = 0;
+			//Write_Back_Sel = 0;
+			//Mem_Write = 0;
+			//Mem_Read = 0;
+			ALU_OP = 6;
+			//ALU_Src = 0;
+			Reg_Write = 1;
+			Write_Reg_Sel = 1;
 		end
 		5'b11010: begin //SRL
 			PC_Write = 1;
-			PC_Code = 0;
-			Comp_Code = 0;
-			Write_Back_Sel = 0;
-			Mem_Write = 0;
-			Mem_Read = 0;
-			ALU_OP = 0;
-			ALU_Src = 0;
-			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			//PC_Code = 0;
+			//Comp_Code = 0;
+			//Write_Back_Sel = 0;
+			//Mem_Write = 0;
+			//Mem_Read = 0;
+			ALU_OP = 7;
+			//ALU_Src = 0;
+			Reg_Write = 1;
+			Write_Reg_Sel = 1;
 		end
 		5'b11100: begin //SEQ
 			PC_Write = 1;
@@ -355,10 +387,10 @@ always @ (OP_Code) begin
 			Write_Back_Sel = 0;
 			Mem_Write = 0;
 			Mem_Read = 0;
-			ALU_OP = 0;
+			ALU_OP = 8;
 			ALU_Src = 0;
 			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			Write_Reg_Sel = 1;
 		end
 		5'b11101: begin //SLT
 			PC_Write = 1;
@@ -367,10 +399,10 @@ always @ (OP_Code) begin
 			Write_Back_Sel = 0;
 			Mem_Write = 0;
 			Mem_Read = 0;
-			ALU_OP = 0;
+			ALU_OP = 9;
 			ALU_Src = 0;
 			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			Write_Reg_Sel = 1;
 		end
 		5'b11110: begin //SLE
 			PC_Write = 1;
@@ -382,7 +414,7 @@ always @ (OP_Code) begin
 			ALU_OP = 0;
 			ALU_Src = 0;
 			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			Write_Reg_Sel = 1;
 		end
 		5'b11111: begin //SCO
 			PC_Write = 1;
@@ -394,7 +426,7 @@ always @ (OP_Code) begin
 			ALU_OP = 0;
 			ALU_Src = 0;
 			Reg_Write = 0;
-			Write_Reg_Sel = 0;
+			Write_Reg_Sel = 1;
 		end
 		5'b01100: begin //BEQZ
 			PC_Write = 1;
