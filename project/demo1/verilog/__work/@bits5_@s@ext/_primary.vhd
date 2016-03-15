1dc64993e4c0993e6debc8e5cf023d607ca09ca4
Cirrith <rbambrough@tux-70.cae.wisc.edu>
2016-03-15 16:17:13 -0500

    Verilog fin

--- project/demo1/verilog/__work/@bits5_@s@ext/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity bits5_sext is
-    port(
-        \In\            : in     vl_logic_vector(4 downto 0);
-        \Out\           : out    vl_logic_vector(15 downto 0)
-    );
-end bits5_sext;
--- project/demo1/verilog/__work/@bits8_@s@ext/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity bits8_sext is
-    port(
-        \In\            : in     vl_logic_vector(7 downto 0);
-        \Out\           : out    vl_logic_vector(15 downto 0)
-    );
-end bits8_sext;
