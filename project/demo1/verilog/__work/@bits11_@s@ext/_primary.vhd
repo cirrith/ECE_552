dc77b188e6b9b185ae183e1a10b3160abb06bda1
Cirrith <rbambrough@tux-13.cae.wisc.edu>
2016-03-15 19:29:04 -0500

    Final Sync

--- project/demo1/verilog/__work/@bits11_@s@ext/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity bits11_sext is
-    port(
-        \In\            : in     vl_logic_vector(10 downto 0);
-        \Out\           : out    vl_logic_vector(15 downto 0)
-    );
-end bits11_sext;
--- project/demo1/verilog/__work/@p@c_inc/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity pc_inc is
-    port(
-        curr_pc         : in     vl_logic_vector(15 downto 0);
-        inc_pc          : out    vl_logic_vector(15 downto 0)
-    );
-end pc_inc;
